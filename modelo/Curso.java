db2f273d2d25efddc0ac286b3770547fc22d54ac
cristian gil <cristianingeniero2018@hotmail.com>
2019-12-26 01:24:26 -0500

    Aplicación para el manejo de los cursos de estudiantes

---
+++ modelo/Curso.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author oncel
+ */
+class Curso {
+    
+    private String codigoCurso;
+    private String nombreCurso;
+    private int creditos;
+    private double nota;
+    
+    Curso(String cod, String non, int cred){
+        this.codigoCurso = cod;
+        this.nombreCurso = non;
+        this.creditos = cred;
+    }
+
+    public String getNombreCurso() {
+        return nombreCurso;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    public String getDescripcion(){
+        return "";
+    }
+    public boolean estaCalificado(){
+        return false;
+    }
+}
